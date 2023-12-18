7acb03ef143295bee9b2f965d47414773a3d7451
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-12-18 21:10:08 +0600

    socket.io resolved

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import { app } from "./app.js";
-import http from "http";
-import { Server } from "socket.io";
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import { app } from './app.js'
+import http from 'http'
+import { Server } from 'socket.io'
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: './.env' })
 
-const port = process.env.PORT;
-const server = http.createServer(app);
-const io = new Server(server,{
+const port = process.env.PORT
+const server = http.createServer(app)
+const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
   },
-});
+})
 
 // Socket.io connection handling
-io.on("connection", (socket) => {
-  console.log("A user connected");
+io.on('connection', (socket) => {
+  console.log('A user connected')
 
-  socket.on("join", ({ sender, receiver }) => {
-    const room = `${sender}-${receiver}`;
-    socket.join(room);
-    console.log(`User ${sender} joined the chat with ${receiver}`);
-  });
+  socket.on('join', (currentUserId, fn) => {
+    socket.join(currentUserId)
+    fn([
+      {
+        sender: 'erer',
+        receiver: 'erer',
+        text: 'erer',
+      },
+    ])
+  })
 
-  socket.on("message", (message) => {
+  socket.on('leave', (currentUserId) => {
+    socket.leave(currentUserId)
+  })
+
+  socket.on('message', (message) => {
+    console.log('Message Received: ', message)
+
     try {
       // Broadcast the message to the specific room
-      const room = `${message.sender}-${message.receiver}`; 
-      console.log("message",message)
-      io.to(room).emit("message", message);
+      io.to(message.receiver).emit('message', message)
     } catch (error) {
-      console.error("Error broadcasting message:", error);
+      console.error('Error broadcasting message:', error)
     }
-  });
+  })
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
-
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+})
 
 connectDB()
   .then(() => {
     server.listen(port, () => {
-      console.log(`Server is running at 👉 http://localhost:${port}`);
-    });
+      console.log(`Server is running at 👉 http://localhost:${port}`)
+    })
   })
   .catch((error) => {
-    console.log("MongoDB Connection Failed !! ", error);
-  });
-
-
+    console.log('MongoDB Connection Failed !! ', error)
+  })
